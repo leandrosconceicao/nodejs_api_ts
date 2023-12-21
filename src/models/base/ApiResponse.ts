d890a6d9905fc6cb87026c7cea5600a4020c641a
leandrosconceicao <leandrosantosconceicao@gmail.com>
2023-12-21 16:48:59 -0300

    Implementação da classe padrão de retorno de api

---
+++ src/models/base/ApiResponse.ts
@@ -0,0 +1,72 @@
+import express from "express";
+
+type ApiInfo<T> = {
+    statusProcess: boolean;
+    message: string;
+    status: number;
+    tecnical?: string;
+    dados?: T;
+}
+
+export default class ApiResponse<T={}> extends Error {
+    response: ApiInfo<T>;
+    constructor(response: ApiInfo<T>) {
+        super();
+        this.response = response;
+    }
+
+    static success<T>(data?: T) : ApiResponse<T> {
+        return new ApiResponse<T>({
+            statusProcess: false,
+            message: "Success",
+            status: 200,
+            dados: data ?? null
+        });
+    }
+
+    static unknow(tecnical?: string) : ApiResponse {
+        return new ApiResponse({
+            statusProcess: false,
+            message: "O servidor não conseguiu processar a requisição, estaremos analisando o caso",
+            tecnical: tecnical,
+            status: 500
+        });
+    }
+
+    static badRequest(message?: string) : ApiResponse {
+        return new ApiResponse({
+            statusProcess: false,
+            message: message ?? "Requisição é inválida.",
+            status: 400
+        });
+    }
+
+    static invalidParameter(parameter?: string) : ApiResponse {
+        return new ApiResponse({
+            statusProcess: false,
+            message: `Parametro obrigatório é inválido ou não foi informado (${parameter})`,
+            status: 406
+        });
+    }
+
+    static unauthorized() : ApiResponse {
+        return new ApiResponse({
+            statusProcess: false,
+            message: `Token inválido ou não informado`,
+            status: 401
+        });
+    }
+
+    static notFound() : ApiResponse {
+        return new ApiResponse({
+            statusProcess: false,
+            message: `Recurso não existe ou não foi localizado`,
+            status: 404
+        });
+    }
+
+    send(res: express.Response) {
+        res.status(this.response.status).json(this);
+    }
+    
+}
