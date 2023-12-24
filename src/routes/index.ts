23802f355c7d738d28d9e815b63bca56cbe131cd
Leandro <leandrosantosconceicao@gmail.com>
2023-12-24 18:56:17 -0300

    Novas rotas

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,20 @@
-import express from "express";
+import express, { NextFunction } from "express";
 import ApiResponse from "../models/base/ApiResponse";
+import userRoutes from "./userRoutes";
+import establishmentsRoutes from "./establishmentsRoutes";
 
 export default (app: express.Application) => {
-    app.get("/", (req: express.Request, res: express.Response) => {
-        ApiResponse.success().send(res);
+    app.get("/", (_: express.Request, res: express.Response, next: NextFunction) => {
+        try {
+            ApiResponse.success().send(res); 
+        } catch (e) {
+            next(e);
+        }
     })
 
     app.use(
-        express.json()
+        express.json(),
+        userRoutes,
+        establishmentsRoutes
     )
 }
