c26a917f3c2876ce7257f632d87edfbfdd212841
Leandro <leandrosantosconceicao@gmail.com>
2023-12-24 18:56:54 -0300

    Configurando middlewares

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import db from "./config/db";
 import router from "./routes/index";
+import pageNotFound from "./middlewares/pageNotFound";
+import errorCatcher from "./middlewares/errorCatcher";
 
 db.on("error", console.log.bind(console, "Erro de conexão"));
 db.once("open", () => console.log("Conexão com o banco feita com sucesso"));
@@ -9,12 +11,10 @@ db.once("open", () => console.log("Conexão com o banco feita com sucesso"));
 const app = express();
 
 app.use(cors({ origin: "*" }));
-app.use(express.json());
 
 router(app);
 
-// app.get("/", (req, res) => {
-//     res.send(200);
-// })
+app.use(pageNotFound)
+app.use(errorCatcher)
 
 export default app;
