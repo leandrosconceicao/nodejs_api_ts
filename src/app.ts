b976eb253fad06d66113574bb6da97a7f499acd9
Leandro <leandrosantosconceicao@gmail.com>
2024-03-24 21:06:45 -0300

    Adicionando novas configurações

--- src/app.ts
+++ src/app.ts
@@ -4,13 +4,29 @@ import db from "../config/db";
 import router from "./routes/index";
 import pageNotFound from "./middlewares/pageNotFound";
 import errorCatcher from "./middlewares/errorCatcher";
+import { initializeApp, cert } from "firebase-admin/app";
+import serviceAccount from '../firebase_config.json';
+import * as dotenv from "dotenv";
+import bodyParse from "body-parser";
+
+dotenv.config();
+
+const storageDB = process.env.FIREBASESTORAGE;
 
 db.on("error", console.log.bind(console, "Erro de conexão"));
 db.once("open", () => console.log("Conexão com o banco feita com sucesso"));
 
+initializeApp({
+    credential: cert(serviceAccount as any),
+    storageBucket: storageDB
+})
+
 const app = express();
 
 app.use(cors({ origin: "*" }));
+app.use(bodyParse.json({
+    limit: "200mb"
+}))
 
 router(app);
 
