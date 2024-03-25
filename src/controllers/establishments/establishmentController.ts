f23c08c269a6ea5b624c769613aa10cf19457be2
Leandro <leandrosantosconceicao@gmail.com>
2024-03-24 21:08:02 -0300

    Ajustes no processo do metodo put e atualização de logo marca para utilizar o serviço de upload de imagens

--- src/controllers/establishments/establishmentController.ts
+++ src/controllers/establishments/establishmentController.ts
@@ -1,11 +1,14 @@
 import { Request, Response, NextFunction } from "express";
 import { Validators } from "../../utils/validators";
-import Establishments from "../../models/Establishments";
-import { Query } from "mongoose";
+import {Establishments, establishmentAttributes } from "../../models/Establishments";
+import {z} from "zod";
 import NotFoundError from "../../models/errors/NotFound";
 import ApiResponse from "../../models/base/ApiResponse";
 import InvalidParameter from "../../models/errors/InvalidParameters";
 import establishmentDataCheck from "./establishmentDataCheck";
+import FirebaseStorage from "../../utils/firebase/storage";
+import fs from "fs/promises";
+
 
 export default class EstablishmentsController {
 
@@ -30,6 +33,7 @@ export default class EstablishmentsController {
     static async findOne(req: Request, res: Response, next: NextFunction) {
         try {
             const id = req.params.id;
+            z.string().min(24).max(24).parse(id);
             const establishment = await Establishments.findById(id);
             if (!establishment) {
                 throw new NotFoundError();
@@ -42,7 +46,7 @@ export default class EstablishmentsController {
 
     static async add(req: Request, res: Response, next: NextFunction) {
         try {
-            const newEst = new Establishments(req.body);
+            const newEst = new Establishments(establishmentAttributes.parse(req.body));
             await newEst.save();
             return ApiResponse.success(newEst).send(res);
         } catch (e) {
@@ -63,6 +67,38 @@ export default class EstablishmentsController {
             }
             const process = await Establishments.findByIdAndDelete(id);
             return ApiResponse.success(process.value).send(res);
+        } catch (e) {
+            next(e);
+        }
+    }
+
+    static async put(req: Request, res: Response, next: NextFunction) {
+        try {
+            const id = req.params.id;
+            z.string().min(24).max(24).parse(id);
+            const establishments = establishmentAttributes.parse(req.body);
+            const process = await Establishments.findByIdAndUpdate(id, establishments, {
+                new: true
+            });
+            return ApiResponse.success(process).send(res);
+        } catch (e) {
+            next(e);
+        }
+    }
+
+    static async updateLogo(req: Request, res: Response, next: NextFunction) {
+        try {
+            const id = z.string().min(24).max(24).parse(req.params.id);
+            const data = z.object({
+                path: z.string().min(1),
+                data: z.string().min(1),
+            }).strict().parse(req.body);
+            const fireSrv = new FirebaseStorage();
+            const imageLink = await fireSrv.uploadFile(data);
+            const process = await Establishments.findByIdAndUpdate(id, {
+                logo: imageLink
+            }, {new: true});
+            return ApiResponse.success(process).send(res);
         } catch (e) {
             next(e);
         }
