ee403049c8a79cf6d4f4794a44abf04eae9fc5d7
Leandro <leandrosantosconceicao@gmail.com>
2024-03-24 21:14:17 -0300

    Refatoração dos metodos

--- src/routes/establishmentsRoutes.ts
+++ src/routes/establishmentsRoutes.ts
@@ -6,6 +6,8 @@ import paginationAndFilters from "../middlewares/paginationAndFilters";
 
 export default Router()
     .get(Endpoints.establishments, validateToken, EstablishmentsController.findAll, paginationAndFilters)
-    .get(`${Endpoints.establishments}/:id`, validateToken, EstablishmentsController.findOne)
+    .get(`${Endpoints.establishments}/:id`, EstablishmentsController.findOne)
     .post(Endpoints.establishments, validateToken, EstablishmentsController.add)
     .delete(Endpoints.establishments, validateToken, EstablishmentsController.delete)
+    .put(`${Endpoints.establishments}/:id`, EstablishmentsController.put)
+    .patch(`${Endpoints.establishments}/upload_logo/:id`, EstablishmentsController.updateLogo)
