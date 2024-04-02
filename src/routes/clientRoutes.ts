552f6cc3ba4509c38aee90e6e2b5cb673cf101a0
Leandro <leandrosantosconceicao@gmail.com>
2024-04-01 21:25:37 -0300

    Exclusão de arquivos não utilizados

--- src/routes/clientRoutes.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import ClientsController from "../controllers/clients/clientsController";
-import Endpoints from "../models/Endpoints";
-import validateToken from "../middlewares/tokenController";
-import paginationAndFilters from "../middlewares/paginationAndFilters";
-
-export default express.Router()
-    .get(Endpoints.clients, validateToken, ClientsController.findAll, paginationAndFilters)
