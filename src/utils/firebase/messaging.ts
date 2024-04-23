3a12d6e8c2802636e3a0ac501895e38bdae29956
Leandro <leandrosantosconceicao@gmail.com>
2024-04-22 21:50:30 -0300

    Implementação para envio de mensagens do firebase

---
+++ src/utils/firebase/messaging.ts
@@ -0,0 +1,41 @@
+import { messaging } from "firebase-admin";
+import { NotificationMessagePayload } from "firebase-admin/lib/messaging/messaging-api";
+
+
+export default class FirebaseMessaging {
+    async sendToUser(
+        token: string, 
+        notification: NotificationMessagePayload,
+        data?: {
+            [key: string]: string;
+        }
+    ) : Promise<string> {
+        try {
+            const instance = messaging();
+            return await instance.send({
+                token: token,
+                data: data,
+                notification: notification,
+                android: {
+                    priority: "high",
+                },
+                // Add APNS (Apple) config
+                apns: {
+                    payload: {
+                        aps: {
+                            contentAvailable: true,
+                        },
+                    },
+                    headers: {
+                        "apns-push-type": "background",
+                        "apns-priority": "5", // Must be `5` when `contentAvailable` is set to true.
+                        "apns-topic": "io.flutter.plugins.firebase.messaging", // bundle identifier
+                    },
+                },
+            })
+        } catch (e) {
+            console.log(e);
+            return undefined;
+        }
+    }
+}
