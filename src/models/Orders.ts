3fdc5c09925d8889fd04ad3da1d17ce9d32c0ed2
Leandro <leandrosantosconceicao@gmail.com>
2024-05-01 22:07:19 -0300

    Validação de schema

--- src/models/Orders.ts
+++ src/models/Orders.ts
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import {z} from "zod";
 import orders_products_schema from "./orders/orders_products";
 import orders_delivery_address from "./orders/orders_delivery_address";
 import { clientsSchema } from "./Clients";
@@ -52,6 +53,62 @@ const orderSchema = new mongoose.Schema({
   dateDiff: {}
 });
 
+const orderValidation = z.object({
+  pedidosId: z.number().optional(),
+  accountId: z.string().min(1).max(24).optional(),
+  accepted: z.boolean().optional(),
+  status: z.enum(['pending', 'cancelled', 'finished', 'onTheWay']).optional(),
+  orderType: z.enum(['frontDesk', 'account', 'delivery', 'withdraw']).optional(),
+  isPayed: z.boolean().optional(),
+  client: z.object({
+      cgc: z.string().optional(),
+      name: z.string().optional(),
+      email: z.string().optional(),
+      phoneNumber: z.string().optional(),
+      address: z.array(z.object({
+          address: z.string().optional(),
+          city: z.string().optional(),
+          complement: z.string().optional(),
+          district: z.string().optional(),
+          number: z.string().optional(),
+          state: z.string().optional(),
+          zipCode: z.string().optional()
+      })).optional(),
+  }),
+  userCreate: z.string().min(1).max(24).optional(),
+  observations: z.string().optional(),
+  storeCode: z.string().min(1).max(24),
+  payment: z.object({
+      accountId: z.string().min(1).max(24).optional(),
+      refunded: z.boolean().optional(),
+      storeCode: z.string().min(1).max(24),
+      userCreate: z.string().min(1).max(24),
+      userUpdated: z.string().min(1).max(24).optional(),
+      value: z.object({
+          txId: z.string().optional(),
+          cardPaymentId: z.string().optional(),
+          value: z.number(),
+          form: z.enum(["money", "debit", "credit", "pix"]).optional(),
+      })
+  }).optional(),
+  products: z.array(z.object({
+      quantity: z.number(),
+      productName: z.string().optional(),
+      productId: z.string().min(1).max(24),
+      orderDescription: z.string().optional(),
+      category: z.string().optional(),
+      needsPreparation: z.boolean().optional(),
+      setupIsFinished: z.boolean().optional(),
+      unitPrice: z.number(),
+      addOnes: z.array(z.object({
+          addOneName: z.string(),
+          quantity: z.number(),
+          price: z.number(),
+          name: z.string(),
+      }).optional()).optional()
+  })),
+});
+
 const Orders = mongoose.model("orders", orderSchema);
 
-export {Orders, orderSchema};
+export {Orders, orderSchema, orderValidation};
