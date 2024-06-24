75161b19b7b2a619db732599e903dd0c5c5d3866
Leandro <leandrosantosconceicao@gmail.com>
2024-06-24 15:25:10 -0300

    Implementações para deixar o nome do app em lowercase

--- src/models/Apps.ts
+++ src/models/Apps.ts
@@ -4,7 +4,9 @@ export default mongoose.model("apps", new mongoose.Schema({
     appsName: {
         type: String,
         unique: true,
-        required: [true, "Parametro (appsName) é obrigatório"]
+        required: [true, "Parametro (appsName) é obrigatório"],
+        lowercase: true, 
+        trim: true
     },
     version: {
         type: String,
