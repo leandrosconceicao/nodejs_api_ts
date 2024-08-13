7a69a43434bf21e5eb8b57abc3f27cc8b740e859
Leandro <leandrosantosconceicao@gmail.com>
2024-08-12 22:30:31 -0300

    Refatorações e correções

--- src/models/CashRegister.ts
+++ src/models/CashRegister.ts
@@ -1,17 +1,18 @@
 import mongoose from 'mongoose';
 import { idValidation } from '../utils/defaultValidations';
 import z from 'zod';
+import { DateQuery, PeriodQuery } from '../utils/PeriodQuery';
+import { ArraySearch, ElementMatch } from './base/MongoDBFilters';
 var ObjectId = mongoose.Types.ObjectId;
 
 const movementSchema = new mongoose.Schema({
     value: {
-        type: mongoose.Types.Decimal128,
+        type: Number,
         required: [true, "O valor do movimento deve ser informado"]
     },
-    paymentMethodId: {
-        type: ObjectId,
-        required: [true, "Informe o ID da forma de pagamento"],
-        ref: "paymentMethods"
+    description: {
+        type: String,
+        required: [true, "Informe a descrição da movimentação"]
     },
     type: {
         type: String,
@@ -20,7 +21,9 @@ const movementSchema = new mongoose.Schema({
             values: ["supply", "withdraw"],
             message: "O type {VALUE} não é um valor permitido"
         }
-    },
+    }
+}, {
+    timestamps: true
 });
 
 const cashMovementValidation = z.object({
@@ -29,7 +32,7 @@ const cashMovementValidation = z.object({
     }, {
         message: "Valor não pode ser 0"
     }),
-    paymentMethodId: idValidation,
+    description: z.string().min(1),
     type: z.enum(["supply", "withdraw"])
 })
 
@@ -38,17 +41,51 @@ const cashRegisterCreationValidation = z.object({
     created_by: idValidation,
     openAt: z.string().datetime({offset: true}).optional(),
     closedAt: z.string().datetime({offset: true}).optional(),
-    // status: z.enum(["open", "closed"]),
     movements: z.array(cashMovementValidation)
 })
 
 const cashRegisterValidationOptional = z.object({
-    storeCode: idValidation.optional(),
+    storeCode: idValidation,
     created_by: idValidation.optional(),
-    openAt: z.string().datetime({offset: true}).optional(),
-    closedAt: z.string().datetime({offset: true}).optional(),
+    openAtStart: z.string().datetime({offset: true}).optional(),
+    openAtEnd: z.string().datetime({offset: true}).optional(),
+    closedAtStart: z.string().datetime({offset: true}).optional(),
+    closedAtEnd: z.string().datetime({offset: true}).optional(),
     status: z.enum(["open", "closed"]).optional(),
-    movements: z.array(cashMovementValidation).optional()
+    deleted: z.object({}).optional(),
+    movement_type: z.enum(["withdraw", "supply"]).optional()
+}).transform((data) => {
+    interface QuerySearch {
+        storeCode: string,
+        created_by?: string,
+        openAt: DateQuery,
+        closedAt: DateQuery,
+        status?: string,
+        deleted: any,
+        movements?: ElementMatch
+    };
+    let query = <QuerySearch> {
+        storeCode: data.storeCode,
+        deleted: data.deleted,
+    };
+    if (data.created_by) {
+        query.created_by = data.created_by;
+    }
+    if (data.status) {
+        query.status = data.status;
+    }
+    if (data.openAtStart && data.openAtEnd) {
+        query.openAt = new PeriodQuery(data.openAtStart, data.openAtEnd).build()
+    }
+    if (data.closedAtStart && data.closedAtEnd) {
+        query.closedAt = new PeriodQuery(data.closedAtStart, data.closedAtEnd).build()
+    }
+    if (data.movement_type) {
+        query.movements = new ArraySearch({
+            type: data.movement_type
+        }).build()
+    }
+    return query;
 })
 
 const cashRegisterSchema = new mongoose.Schema({
@@ -64,7 +101,7 @@ const cashRegisterSchema = new mongoose.Schema({
     },
     openAt: {
         type: Date,
-        default: () => { new Date() }
+        default: () => { return new Date() }
     },
     closedAt: {
         type: Date
@@ -79,10 +116,14 @@ const cashRegisterSchema = new mongoose.Schema({
     },
     movements: {
         type: [movementSchema],
+    },
+    deleted: {
+        type: ObjectId,
+        default: undefined
     }
 });
 
 const CashRegister = mongoose.model('cashRegister', cashRegisterSchema);
 
 
-export { CashRegister, cashRegisterCreationValidation, cashMovementValidation };
+export { CashRegister, cashRegisterCreationValidation, cashMovementValidation, cashRegisterValidationOptional };
