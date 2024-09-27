8cc3831f7505dc9977618692573747bea6767161
Leandro <leandrosantosconceicao@gmail.com>
2024-09-26 23:53:32 -0300

    Extração e refatoração de dados

--- src/models/products/Products.ts
+++ src/models/products/Products.ts
@@ -1,4 +1,37 @@
 import mongoose from "mongoose";
+import { idValidation } from "../../utils/defaultValidations";
+import {z} from "zod";
+
+var ObjectId = mongoose.Schema.Types.ObjectId;
+
+const PRODUCT_SCHEMA_VALIDATION = z.object({
+  isActive: z.boolean().optional(),
+  storeCode: idValidation,
+  category: idValidation.optional(),
+  preco: z.number().optional(),
+  produto: z.string().min(1).optional(),
+  tipValue: z.number().optional(),
+  descricao: z.string().optional(),
+  preparacao: z.boolean().optional(),
+  dataImage: z.object({
+      path: z.string().min(1),
+      data: z.string().min(1),
+  }).optional(),
+  image: z.string().optional(),
+  addOnes: z.array(z.object({
+      isRequired: z.boolean(),
+      productsAddOnes: z.object({
+          _id: z.string(),
+          name: z.string(),
+          // price: z.number(),
+          maxQtdAllowed: z.number(),
+          items: z.array(z.object({
+              name: z.string(),
+              price: z.number()
+          }))
+      })
+  })).optional(),
+});
 
 const productSchema = new mongoose.Schema({
   // _id: { type: Number },
@@ -6,7 +39,11 @@ const productSchema = new mongoose.Schema({
     required: [true, "Parametro (isActive) é obrigatório"] 
   },
   category: {
-    type: mongoose.Schema.Types.ObjectId, ref: "categories", required: true
+    type: ObjectId, ref: "categories", required: true
+  },
+  tipValue: {
+    type: Number,
+    default: 0.0
   },
   preco: { type: Number },
   produto: { type: String, 
@@ -15,7 +52,7 @@ const productSchema = new mongoose.Schema({
   createDate: {type: Date, default: () => { return new Date() }},
   descricao: { type: String },
   preparacao: Boolean,
-  storeCode: {type: mongoose.Types.ObjectId, ref: "establishments", required: [true, "Parametro (storeCode) é obrigatório"] },
+  storeCode: {type: ObjectId, ref: "establishments", required: [true, "Parametro (storeCode) é obrigatório"] },
   addOnes: {
     type: [
       {
@@ -45,4 +82,4 @@ const productSchema = new mongoose.Schema({
 
 const Products = mongoose.model("products", productSchema);
 
-export {productSchema, Products};
+export {productSchema, PRODUCT_SCHEMA_VALIDATION, Products};
