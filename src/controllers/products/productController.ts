9aa586306c1f1a35db19e5cbd5bf62d910d4232f
Leandro <leandrosantosconceicao@gmail.com>
2024-09-26 23:51:05 -0300

    Refatorações

--- src/controllers/products/productController.ts
+++ src/controllers/products/productController.ts
@@ -1,8 +1,9 @@
 import { Validators } from "../../utils/validators";
 import { z } from "zod";
 import ApiResponse from "../../models/base/ApiResponse";
+import { idValidation } from "../../utils/defaultValidations";
 import NotFoundError from "../../models/errors/NotFound";
-import { Products } from "../../models/products/Products";
+import { PRODUCT_SCHEMA_VALIDATION, Products } from "../../models/products/Products";
 import { Request, Response, NextFunction } from "express";
 import InvalidParameter from "../../models/errors/InvalidParameters";
 import mongoose, { ObjectId } from "mongoose";
@@ -52,7 +53,7 @@ export default class ProductController {
 
     static async findOne(req: Request, res: Response, next: NextFunction) {
         try {
-            let id = req.params.id;
+            const id = idValidation.parse(req.params.id);
             const product = await Products.findById(id);
             if (!product) {
                 throw new NotFoundError("Produto não localizado");
@@ -65,7 +66,8 @@ export default class ProductController {
 
     static async addProduct(req: Request, res: Response, next: NextFunction) {
         try {
-            let newProduct = new Products(req.body);
+            const data = PRODUCT_SCHEMA_VALIDATION.parse(req.body);
+            const newProduct = new Products(data);
             const newProductAdd = await newProduct.save();
             return ApiResponse.success(newProductAdd).send(res);
         } catch (e) {
@@ -75,34 +77,8 @@ export default class ProductController {
 
     static async update(req: Request, res: Response, next: NextFunction) {
         try {
-            const id = z.string().min(1).parse(req.params.id);
-            const product = z.object({
-                isActive: z.boolean().optional(),
-                storeCode: z.string().min(1).max(24),
-                category: z.string().min(1).max(24).optional(),
-                preco: z.number().optional(),
-                produto: z.string().min(1).optional(),
-                descricao: z.string().optional(),
-                preparacao: z.boolean().optional(),
-                dataImage: z.object({
-                    path: z.string().min(1),
-                    data: z.string().min(1),
-                }).optional(),
-                image: z.string().optional(),
-                addOnes: z.array(z.object({
-                    isRequired: z.boolean(),
-                    productsAddOnes: z.object({
-                        _id: z.string(),
-                        name: z.string(),
-                        // price: z.number(),
-                        maxQtdAllowed: z.number(),
-                        items: z.array(z.object({
-                            name: z.string(),
-                            price: z.number()
-                        }))
-                    })
-                })).optional(),
-            }).parse(req.body);
+            const id = idValidation.parse(req.params.id);
+            const product = PRODUCT_SCHEMA_VALIDATION.parse(req.body);
             if (product.dataImage) {
                 const link = await updateImage({
                     path: `assets/${product.storeCode}/${product.dataImage.path}`,
