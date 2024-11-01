81ca47e21a70e71eccaacc6149a96c774b91e863
Leandro <leandrosantosconceicao@gmail.com>
2024-11-01 12:04:11 -0300

    Adicionando middleware para o controle de impressão de pedidos

--- src/routes/ordersRoutes.ts
+++ src/routes/ordersRoutes.ts
@@ -3,12 +3,13 @@ import Endpoints from "../models/Endpoints";
 import validateToken from "../middlewares/tokenController";
 import paginationAndFilters from "../middlewares/paginationAndFilters";
 import OrdersController from "../controllers/orders/ordersController";
+import printerSpoolMiddleware from "../middlewares/printerSpoolMiddleware";
 
 export default express.Router()
     .get(Endpoints.orders, OrdersController.findAll, paginationAndFilters)
     .get(`${Endpoints.orders}/:id`, OrdersController.findOne)
     .get(`${Endpoints.orders}/orders_on_preparation/:id`, OrdersController.getOrdersOnPreparation)
-    .post(Endpoints.orders, OrdersController.newOrder)
+    .post(Endpoints.orders, OrdersController.newOrder, printerSpoolMiddleware)
     .put(`${Endpoints.orders}/cancel_order`, validateToken, OrdersController.cancelOrder)
     .put(`${Endpoints.orders}/set_preparation/:id`, validateToken, OrdersController.setPreparation)
     .patch(`${Endpoints.orders}/transfer`, validateToken, OrdersController.transfer)
