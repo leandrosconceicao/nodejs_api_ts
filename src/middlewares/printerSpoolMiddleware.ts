d48e7ca534cead830b3cbc21101667ae832cf620
Leandro <leandrosantosconceicao@gmail.com>
2024-11-01 12:02:03 -0300

    middleware para interceptar as requisições de criação de pedidos, enviando para o spool de impressão o pedido realizado

---
+++ src/middlewares/printerSpoolMiddleware.ts
@@ -0,0 +1,23 @@
+import { NextFunction, Request, Response } from "express";
+import { IOrder } from "../models/Orders";
+import ApiResponse from "../models/base/ApiResponse";
+import {PrinterSpool} from "../models/PrinterSpool";
+import LogsController from "../controllers/logs/logsController";
+
+export default async function printerSpoolMiddleware(req: Request, res: Response, next: NextFunction) {
+    const order = req.result as IOrder;
+    try {        
+        new PrinterSpool({
+            type: "order",
+            storeCode: order.storeCode,
+            orderId: order._id,
+            accountId: order.accountId,
+        }).save();
+
+    } catch (e) {
+        const log = new LogsController();
+        log.saveReqLog(req, e);
+    } finally {
+        return ApiResponse.success(order).send(res);
+    }
+}
