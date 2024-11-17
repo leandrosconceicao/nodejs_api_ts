a39d8edd7b8a67211c7d5a05c6d93c22ef30094d
Leandro <leandrosantosconceicao@gmail.com>
2024-11-17 11:44:19 -0300

    Refatorações e implementação de middleware para gerenciamento de dados de spool

--- src/middlewares/printerSpoolMiddleware.ts
+++ src/middlewares/printerSpoolMiddleware.ts
@@ -1,25 +1,41 @@
 import { NextFunction, Request, Response } from "express";
 import { IOrder } from "../models/Orders";
 import ApiResponse from "../models/base/ApiResponse";
-import {IPrinterSpool, PrinterSpool} from "../models/PrinterSpool";
+import { IPrinterSpool, SpoolType } from "../models/PrinterSpool";
 import LogsController from "../controllers/logs/logsController";
+import SpoolHandler from "../domain/handlers/spoolHandler";
+import { getDatabase } from "firebase-admin/database";
 
-export default async function printerSpoolMiddleware(req: Request, res: Response, next: NextFunction) {
+async function spoolManagement(req: Request, res: Response, next: NextFunction) {
+    const spool = req.result as IPrinterSpool
+    const handler = new SpoolHandler();
+    try {
+        const data = await handler.prepareData(spool);
+        const db = getDatabase();
+        db.ref(`${data.storeCode}`).child("spool").push(data, (value) => {
+            if (value) {
+                throw value;
+            }
+            ApiResponse.success(data).send(res);
+        });
+    } catch (e) {
+        next(e);
+    }
+}
+
+async function printerSpoolMiddleware(req: Request, res: Response, next: NextFunction) {
     const order = req.result as IOrder;
-    try {        
-        const data = <Partial<IPrinterSpool>>{
-            type: "order",
+    const handler = new SpoolHandler();
+    try {
+        const data = await handler.prepareData({
+            type: SpoolType.order,
             storeCode: `${order.storeCode}`,
             orderId: `${order._id}`,
-        };
-        if (order.accountId) {
-            data.accountId = `${order.accountId}`
-        }
-        new PrinterSpool({
-            storeCode: order.storeCode,
-            orderId: order._id,
-            accountId: order.accountId,
-        }).save();
+            accountId: `${order?.accountId ?? ""}`,
+            reprint: false
+        })
+        const db = getDatabase();
+        db.ref(`${data.storeCode}`).child("spool").push(data);
 
     } catch (e) {
         const log = new LogsController();
@@ -27,3 +43,5 @@ export default async function printerSpoolMiddleware(req: Request, res: Response
     }
     return ApiResponse.success(order).send(res);
 }
+
+export {printerSpoolMiddleware, spoolManagement}
