262365fc1fd6b756810034525662448fed4c3cc7
Leandro <leandrosantosconceicao@gmail.com>
2024-12-01 17:53:33 -0300

    Ajuste de campos e validações

--- src/models/Orders.ts
+++ src/models/Orders.ts
@@ -1,11 +1,13 @@
 import mongoose from "mongoose";
 import {z} from "zod";
 import orders_products_schema from "./orders/orders_products";
-import orders_delivery_address from "./orders/orders_delivery_address";
-import { clientsSchema } from "./Clients";
+import { clientsSchema, IClient } from "./Clients";
 import { idValidation } from "../utils/defaultValidations";
-import { paymentValidation } from "./Payments";
 import MongoId from "./custom_types/mongoose_types";
+import { IUsers } from "./Users";
+import { IAccount } from "./Accounts";
+import { IEstablishments } from "./Establishments";
+import { IPayment } from "./Payments";
 var ObjectId = mongoose.Types.ObjectId;
 
 const orderSchema = new mongoose.Schema({
@@ -14,7 +16,6 @@ const orderSchema = new mongoose.Schema({
   accountId: {
     type: ObjectId, ref: "accounts",
   },
-  createDate: { type: Date , default: () => {return new Date();}},
   updated_at: { type: Date },
   updated_by: { type: ObjectId, ref: "users"},
   orderType: {
@@ -27,7 +28,7 @@ const orderSchema = new mongoose.Schema({
   },
   accepted: { type: Boolean, default: null },
   status: {
-    type: "String",
+    type: String,
     default: "pending",
     enum: {
       values: ['pending', 'cancelled', 'finished', 'onTheWay'],
@@ -36,30 +37,67 @@ const orderSchema = new mongoose.Schema({
   },
   products: {
     type: [
-      orders_products_schema
+      orders_products_schema,
     ],
+    validate: {
+      validator: function (data: Array<any>) {
+        return data.length;
+      },
+      message: ""
+    }
   },
-  isPayed: { type: Boolean, default: false },
   client: {
     type: clientsSchema
   },
-  deliveryAddress: {
-    type: orders_delivery_address,
-  },
-  observations: {type: String, default: ""},
-  userCreate: {
+  createdBy: {
     type: ObjectId, ref: "users",
   },
   storeCode: {type: ObjectId, ref: "establishments" , required: [true, "Parametro (storeCode) é obrigatório"]},
-  payment: {
-    type: mongoose.Types.ObjectId, ref: 'payments'
-  },
   paymentMethod: {
     type: ObjectId,
     ref: "paymentMethods",
   },
-  dateDiff: {}
+}, {
+  timestamps: true
 });
+
+orderSchema.virtual("userCreate", {
+  ref: 'users',
+  localField: 'createdBy',
+  foreignField: '_id',
+  justOne: true
+})
+
+orderSchema.virtual("accountDetail", {
+  ref: 'accounts',
+  localField: 'accountId',
+  foreignField: '_id',
+  justOne: true
+})
+
+orderSchema.virtual("storeCodeDetail", {
+  ref: 'establishments',
+  localField: 'storeCode',
+  foreignField: '_id',
+  justOne: true
+})
+
+orderSchema.virtual("paymentMethodDetail", {
+  ref: 'paymentMethods',
+  localField: 'paymentMethod',
+  foreignField: '_id',
+  justOne: true
+})
+
+orderSchema.virtual("paymentDetail", {
+  ref: 'payments',
+  localField: '_id',
+  foreignField: 'orderId',
+  justOne: true
+})
+
+orderSchema.set('toObject', { virtuals: true });
+orderSchema.set('toJSON', { virtuals: true });
 
 const orderProductValidation = z.object({
   quantity: z.number(),
@@ -69,6 +107,7 @@ const orderProductValidation = z.object({
   category: z.string().optional(),
   needsPreparation: z.boolean().optional(),
   setupIsFinished: z.boolean().optional(),
+  observations: z.string().default(""),
   unitPrice: z.number(),
   tipValue: z.number().optional(),
   hasTipValue: z.boolean().optional(),
@@ -82,12 +121,13 @@ const orderProductValidation = z.object({
 
 const orderValidation = z.object({
   pedidosId: z.number().optional(),
-  accountId: idValidation.optional(),
   firebaseToken: z.string().optional(),
+  accountId: idValidation.optional(),
+  updated_by: idValidation.optional(),
+  orderType: z.enum(['frontDesk', 'account', 'delivery', 'withdraw']).default("frontDesk"),
   accepted: z.boolean().optional(),
-  status: z.enum(['pending', 'cancelled', 'finished', 'onTheWay']).optional(),
-  orderType: z.enum(['frontDesk', 'account', 'delivery', 'withdraw']).optional(),
-  isPayed: z.boolean().optional(),
+  status: z.enum(['pending', 'cancelled', 'finished', 'onTheWay']).default("pending"),
+  products: z.array(orderProductValidation).nonempty(),
   client: z.object({
       cgc: z.string().optional(),
       name: z.string().optional(),
@@ -101,14 +141,16 @@ const orderValidation = z.object({
           number: z.string().optional(),
           state: z.string().optional(),
           zipCode: z.string().optional()
-      })).optional(),
+      })).nonempty().optional(),
+  }).default({
+    cgc: "",
+    name: "",
+    email: "",
+    phoneNumber: "",
   }),
-  userCreate: idValidation.optional(),
-  observations: z.string().optional(),
+  createdBy: idValidation.optional(),
   storeCode: idValidation,
-  payment: paymentValidation.optional(),
   paymentMethod: idValidation.optional(),
-  products: z.array(orderProductValidation).nonempty(),
 });
 
 enum OrderStatus {
@@ -125,134 +167,69 @@ enum OrderType {
   withdraw = 'withdraw'
 }
 
+interface IOrderProduct {
+  quantity: number,
+  productName?: string,
+  productId: string | MongoId,
+  orderDescription?: string,
+  observations: string,
+  category?: string,
+  needsPreparation?: boolean,
+  setupIsFinished?: boolean,
+  unitPrice: number,
+  tipValue?: number,
+  hasTipValue?: boolean,
+  subTotal?: number,
+  addOnes?: Array<{
+    addOneName: string,
+    quantity: number,
+    price: number,
+    name: string,
+  }>      
+}
+
 interface IOrder {
-  _id: MongoId,
+  _id: string | MongoId,
   pedidosId?: number,
   firebaseToken?: string,
-  accountId?: {
-    description?: string
-  },
-  // accepted?: string,
-  status?: OrderStatus,
-  createDate?: Date,
+  accountId: string | MongoId,
   orderType?: OrderType,
-  client?: {
-    cgc?: string,
-    name?: string,
-    email?: string,
-    phoneNumber?: string,
-    address?: Array<{
-      address?: string,
-      city?: string,
-      complement?: string,
-      district?: string,
-      number?: string,
-      state?: string,
-      zipCode?: string,
-    }>
-  },
-  userCreate?: {
-    username: string
-  },
-  observations?: string,
-  storeCode: MongoId,
-  payment?: {
-    accountId?: MongoId,
-    cashRegisterId: MongoId,
-    userCreate: MongoId,
-    value: {
-      method: {
-        description: string,
-        _id: MongoId
-      },
-      value: number
-    }
-  },
-  products: Array<{
-    quantity: number,
-    productName?: string,
-    productId: MongoId,
-    orderDescription?: string,
-    category?: string,
-    needsPreparation?: boolean,
-    setupIsFinished?: boolean,
-    unitPrice: number,
-    tipValue?: number,
-    hasTipValue?: boolean,
-    subTotal?: number,
-    addOnes?: Array<{
-      addOneName: string,
-      quantity: number,
-      price: number,
-      name: string,
-    }>      
-  }>,
+  accepted?: boolean,
+  status?: OrderStatus,
+  products: Array<IOrderProduct>,
+  client?: IClient,
+  createdBy?: string | MongoId,
+  updatedBy?: string | MongoId,
+  storeCode: string | MongoId,
+  paymentMethod: string | MongoId,
+  createdAt?: Date,
+  userCreate?: IUsers,
+  accountDetail?: IAccount, 
+  storeCodeDetail?: IEstablishments,
+  paymentDetail?: IPayment
 }
 
 interface IFirebaseOrder {
   _id: string,
   pedidosId?: number,
   firebaseToken?: string,
-  accountId?: {
-    description?: string
-  },
-  // accepted?: string,
-  status?: OrderStatus,
-  createDate?: string,
+  accountId: string,
   orderType?: OrderType,
-  client?: {
-    cgc?: string,
-    name?: string,
-    email?: string,
-    phoneNumber?: string,
-    address?: Array<{
-      address?: string,
-      city?: string,
-      complement?: string,
-      district?: string,
-      number?: string,
-      state?: string,
-      zipCode?: string,
-    }>
-  },
-  userCreate?: {
-    username: string
-  },
-  observations?: string,
+  accepted?: boolean,
+  status?: OrderStatus,
+  products: Array<Partial<IOrderProduct>>,
+  client?: IClient,
+  createdBy?: string,
+  updatedBy?: string,
   storeCode: string,
-  payment?: {
-    accountId?: string,
-    cashRegisterId: string,
-    userCreate: string,
-    value: {
-      method: {
-        description: string,
-        _id: string
-      },
-      value: number
-    }
-  },
-  products: Array<{
-    quantity: number,
-    productName?: string,
-    productId: string,
-    orderDescription?: string,
-    category?: string,
-    needsPreparation?: boolean,
-    setupIsFinished?: boolean,
-    unitPrice: number,
-    tipValue?: number,
-    hasTipValue?: boolean,
-    subTotal?: number,
-    addOnes?: Array<{
-      addOneName: string,
-      quantity: number,
-      price: number,
-      name: string,
-    }>      
-  }>,
+  paymentMethod: string,
+  createdAt?: string,
+  createDate?: string,
+  userCreate?: Partial<IUsers>,
+  accountDetail?: Partial<IAccount>, 
+  storeCodeDetail?: Partial<IEstablishments>
 }
 
 const Orders = mongoose.model("orders", orderSchema);
 
-export {Orders, orderSchema, orderValidation, orderProductValidation, IOrder, IFirebaseOrder, OrderType, OrderStatus};
+export {Orders, orderSchema, orderValidation, orderProductValidation, IOrder, IFirebaseOrder, OrderType, OrderStatus, IOrderProduct};
