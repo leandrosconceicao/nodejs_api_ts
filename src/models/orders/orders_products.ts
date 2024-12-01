9ea5686948eb7ccdd8dfe01bede0fb6477587f7f
Leandro <leandrosantosconceicao@gmail.com>
2024-12-01 17:55:40 -0300

    Ajuste de campos

--- src/models/orders/orders_products.ts
+++ src/models/orders/orders_products.ts
@@ -2,36 +2,38 @@ import mongoose from "mongoose";
 var ObjectId = mongoose.Types.ObjectId;
  
 const orders_products_schema = new mongoose.Schema({
-    quantity: {type: Number, required: [true, "Parametro (products[].quantity) é obrigatório"]},
-    productName: {type: String, default: ""},
-    productId: {type: ObjectId, required: [true, "Parametro (products[].productId) é obrigatório"]},
-    orderDescription: {type: String, default: ""},
-    category: {type: String, default: ""},
-    needsPreparation: {type: Boolean, default: true},
-    setupIsFinished: {type: Boolean, default: false},
-    tipValue: {
-      type: Number,
-      default: 0.0
-    },
-    unitPrice: {type: Number, required: [true, "Parametro (products[].unitPrice) é obrigatório"]},
-    addOnes: {
-      type: [
-        {
-          addOneName: String,
-          quantity: Number,
-          name: String,
-          price: Number,
-        },
-      ],
-      default: undefined,
-    },
-  })
+  quantity: {type: Number, required: [true, "Parametro (products[].quantity) é obrigatório"]},
+  productName: {type: String, default: ""},
+  productId: {type: ObjectId, required: [true, "Parametro (products[].productId) é obrigatório"]},
+  orderDescription: {type: String, default: ""},
+  category: {type: String, default: ""},
+  needsPreparation: {type: Boolean, default: true},
+  setupIsFinished: {type: Boolean, default: false},
+  observations: {type: String, default: ""},
+  unitPrice: {type: Number, required: [true, "Parametro (products[].unitPrice) é obrigatório"]},
+  tipValue: {
+    type: Number,
+    default: 0.0
+  },
+  addOnes: {
+    type: [
+      {
+        _id: String,
+        addOneName: String,
+        quantity: Number,
+        name: String,
+        price: Number,
+      },
+    ],
+    default: undefined,
+  },
+})
 
-  orders_products_schema.virtual("subTotal")
-    .get(function() {
-      let subTotal = (this.quantity * this.unitPrice);
-      return subTotal + (this.tipValue * subTotal)
-    })
+orders_products_schema.virtual("subTotal")
+  .get(function() {
+    let subTotal = (this.quantity * this.unitPrice);
+    return subTotal + (this.tipValue * subTotal)
+  })
 // orders_products_schema.set('toObject', { virtuals: true });
 // orders_products_schema.set('toJSON', { virtuals: true });
 
