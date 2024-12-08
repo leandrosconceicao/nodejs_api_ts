6c5104d2c0a5ffc3c7688ab06720ca1bcd8984d3
Leandro <leandrosantosconceicao@gmail.com>
2024-12-08 18:54:48 -0300

    Adicionando middlewares

--- src/routes/ordersRoutes.ts
+++ src/routes/ordersRoutes.ts
@@ -3,14 +3,15 @@ import Endpoints from "../models/Endpoints";
 import validateToken from "../middlewares/tokenController";
 import paginationAndFilters from "../middlewares/paginationAndFilters";
 import OrdersController from "../controllers/orders/ordersController";
-import {printerSpoolMiddleware} from "../middlewares/printerSpoolMiddleware";
-import {addPreparationOrder, manageWithDrawMonitor, setOrderOnPreparation, updateWithDrawMonitor} from "../middlewares/ordersMiddleware";
+import {printerSpoolMiddleware, removePrinterSpool} from "../middlewares/printerSpoolMiddleware";
+import {addPreparationOrder, manageWithDrawMonitor, setOrderOnPreparation, updateWithDrawMonitor, removePreparation} from "../middlewares/ordersMiddleware";
+import { deletePaymment } from "../middlewares/paymentsMiddlewares";
 
 export default express.Router()
     .get(Endpoints.orders, OrdersController.findAll, paginationAndFilters)
     .get(`${Endpoints.orders}/:id`, OrdersController.findOne)
     .post(Endpoints.orders, OrdersController.newOrder, addPreparationOrder, manageWithDrawMonitor, printerSpoolMiddleware)
-    .put(`${Endpoints.orders}/cancel_order`, validateToken, OrdersController.cancelOrder)
+    .delete(`${Endpoints.orders}/:id`, validateToken, OrdersController.cancelOrder, deletePaymment, removePreparation, removePrinterSpool)
     .put(`${Endpoints.orders}/set_preparation/:id`, validateToken, OrdersController.setPreparation, updateWithDrawMonitor, setOrderOnPreparation)
     .patch(`${Endpoints.orders}/transfer`, validateToken, OrdersController.transfer)
     .patch(`${Endpoints.orders}/change_seller/:id`, validateToken, OrdersController.changeSeller)
