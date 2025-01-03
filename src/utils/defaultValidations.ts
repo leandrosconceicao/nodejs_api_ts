a1340083ca4b9daf652cf1482f5d41c9a2e91d9d
Leandro <leandrosantosconceicao@gmail.com>
2025-01-03 12:59:55 -0300

    Validação de input de número decimal

--- src/utils/defaultValidations.ts
+++ src/utils/defaultValidations.ts
@@ -8,10 +8,18 @@ const idValidation = z.string().refine(value => {
     message: "ID inválido"
 });
 
+function decimalValidation(message: string) {
+    return z.number().refine(value => {
+        return value === 0.0 || value === 1.0 || !Number.isInteger(value);
+        }, {
+        message: message
+    });
+}
+
 const booleanStringValidation = z.string().toLowerCase()
     .refine((value) => value === "true" || value === "false", {
     message: "Value must be a boolean",
     })
     .transform((value) => value === "true");
 
-export {idValidation, booleanStringValidation};
+export {idValidation, booleanStringValidation, decimalValidation};
