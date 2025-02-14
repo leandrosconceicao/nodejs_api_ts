619bb44db743cd66aee3f675597df15ffc8cb6da
Leandro <leandrosantosconceicao@gmail.com>
2025-02-14 11:22:51 -0300

    Ajustando tipos e propriedades

--- src/models/products/AddOnes.ts
+++ src/models/products/AddOnes.ts
@@ -3,27 +3,51 @@ import { idValidation } from "../../utils/defaultValidations";
 import mongoose from "mongoose"
 import { z } from "zod";
 
+enum AddOneType {
+    checkbox = "checkbox",
+    range = "range"
+}
+
+interface IProductAddOne {
+    _id: string,
+    isRequired: boolean,
+    name: string,
+    type: AddOneType,
+    maxQtdAllowed: number,
+    items: Array<{
+        name: string,
+        price: number
+    }>
+}
+
 const addOneValidation = z.object({
     _id: z.string().min(1).uuid().optional(),
     storeCode: idValidation,
     name: z.string(),
-    price: z.number().default(0.0),
-    maxQtdAllowed: z.number(),
+    type: z.nativeEnum(AddOneType).default(AddOneType.checkbox),
+    maxQtdAllowed: z.number().optional(),
     items: z.array(z.object({
         name: z.string().min(1),
         price: z.number().default(0.0)
-    })).nonempty()
-  }).transform((value) => {
+    })).nonempty({ message: "Ao menos 1 item deve ser adicionado" })
+}).transform((value) => {
     value._id = TokenGenerator.generateId();
     return value;
-  });
+});
 
 const schema = new mongoose.Schema({
     _id: { type: String },
     storeCode: { type: mongoose.Types.ObjectId, ref: "establishments", required: [true, "Parametro (storeCode) é obrigatório"] },
     name: { type: String },
-    price: { type: Number },
     maxQtdAllowed: { type: Number },
+    type: {
+        type: String,
+        default: "checkbox",
+        enum: {
+            values: ["checkbox", "range"],
+            message: "O tipo {VALUE} não é um valor permitido"
+        }
+    },
     items: {
         type: [
             {
@@ -45,7 +69,7 @@ schema.index({
 })
 
 
-const AddOnes = mongoose.model("addOnes", schema);
+const AddOnes = mongoose.model<IProductAddOne>("addOnes", schema);
 
-  
-export {addOneValidation, AddOnes}
+
+export { addOneValidation, AddOnes, AddOneType, IProductAddOne }
