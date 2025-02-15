81bf48f6057df3c892512cbf793501700038836f
Leandro <leandrosantosconceicao@gmail.com>
2025-02-15 11:42:01 -0300

    Refatorando o controller para utilizar a injeção de dependencias

--- src/controllers/products/categoriesController.ts
+++ src/controllers/products/categoriesController.ts
@@ -1,49 +1,42 @@
 import mongoose from "mongoose";
-import { Validators } from "../../utils/validators";
-import {Category} from "../../models/Categories";
+import {categorySchemaValidation, ICategory} from "../../models/Categories";
 import ApiResponse from "../../models/base/ApiResponse";
-import ProductController from "./productController";
 import { Request, Response, NextFunction } from "express";
-import { RegexBuilder } from "../../utils/regexBuilder";
-import InvalidParameter from "../../models/errors/InvalidParameters";
 import NotFoundError from "../../models/errors/NotFound";
-var ObjectId = mongoose.Types.ObjectId;
+import ICategoryRepository from "../../domain/interfaces/ICategoryRepository";
+import { z } from "zod";
+import { idValidation } from "../../utils/defaultValidations";
+import BadRequestError from "../../models/errors/BadRequest";
+import { autoInjectable, inject } from "tsyringe";
 
-interface SearchFilter {
-  _id?: string,
-  storeCode?: mongoose.Types.ObjectId,
-  nome?: RegExp
-}
+@autoInjectable()
 class CategoryController {
 
-  static async findAll(req: Request, res: Response, next: NextFunction) {
+  constructor(
+    @inject("ICategoryRepository") private readonly repository : ICategoryRepository
+  ) {}
+
+  findAll = async (req: Request, res: Response, next: NextFunction) => {
     try {
-      let { id, storeCode, nome } = req.query;
-      let cat = <SearchFilter>{};
-      if (new Validators("id", id).validate().isValid) {
-        cat._id = id as string;
-      }
-      if (new Validators("storeCode", storeCode).validate().isValid) {
-        cat.storeCode = new ObjectId(storeCode as string);
-      }
-      if (new Validators("nome", nome).validate().isValid) {
-        cat.nome = RegexBuilder.searchByName(nome as string);
-      }
-      req.result = Category.find(cat);
+      const query = z.object({
+        storeCode: idValidation,
+        id: idValidation.optional(),
+        nome: z.string().min(1).optional()
+      }).parse(req.query);
+
+      req.result = this.repository.findAll(query.storeCode, query.id, query.nome);
       next();
     } catch (e) {
       next(e);
     }
   }
 
-  static async findOne(req: Request, res: Response, next: NextFunction) {
+  findOne = async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const id = req.params.id;
-      const validation = new Validators("id", id, "string").validate();
-      if (!validation.isValid) {
-        throw new InvalidParameter(validation);
-      }
-      const category = await Category.findById(id);
+      const id = idValidation.parse(req.params.id);
+      
+      const category = await this.repository.findOne(id);
+
       if (!category) {
         throw new NotFoundError();
       }
@@ -53,91 +46,89 @@ class CategoryController {
     }
   }
 
-  static async add(req: Request, res: Response, next: NextFunction) {
+  add = async (req: Request, res: Response, next: NextFunction) => {
     try {
-      let category = new Category(req.body);
-      let lastCategory = await CategoryController.getLastCategory(category.storeCode);
-      if (lastCategory) {
-        const fetchDoc = new Category(lastCategory);
-        category.ordenacao = fetchDoc.ordenacao + 1;
-      } else {
-        category.ordenacao = 1;
-      }
-      await category.save();
-      return ApiResponse.success().send(res);
+      const category = categorySchemaValidation
+        .transform((value) => {
+          return <ICategory>{
+            nome: value.nome,
+            storeCode: new mongoose.Types.ObjectId(value.storeCode),
+            image: value.image,
+          };
+        })
+        .parse(req.body);
+
+      const newCategory = await this.repository.add(category);
+      return ApiResponse.success(newCategory, 201).send(res);
     } catch (e) {
       next(e);
     }
   }
 
-  static async getLastCategory(storeCode: mongoose.Types.ObjectId): Promise<mongoose.Document> {
+  update = async (req: Request, res: Response, next: NextFunction) => { 
     try {
-      const categories = await Category.find({ storeCode: storeCode });
-      const data = categories.splice(-1)[0];
-      return data;
-    } catch (e) {
-      return null;
-    }
-  }
+      const id = idValidation.parse(req.params.id);
+      const data = z.object({
+        nome: z.string().min(1).optional(),
+        image: z.string().base64().optional()
+      })
+      .transform((values) => {
+        const body = <Partial<{
+          nome: string,
+          image: string
+        }>>{};
 
-  static async updateName(req: Request, res: Response, next: NextFunction) {
-    try {
-      let { id, name } = req.body;
-      const idValidation = new Validators("id", id, "name").validate();
-      const nameValidation = new Validators("name", name, "string").validate();
-      if (!idValidation.isValid) {
-        throw new InvalidParameter(idValidation);
-      }
-      if (!nameValidation.isValid) {
-        throw new InvalidParameter(nameValidation);
-      }
-      await Category.findByIdAndUpdate(id, { "nome": name })
-      return ApiResponse.success().send(res);
-    } catch (e) {
-      next(e);
-    }
-  }
+        if (values.image) {
+          body.image = values.image;
+        }
 
-  static async changeOrder(req: Request, res: Response, next: NextFunction) {
-    try {
-      const from = req.body.from;
-      const to = req.body.to;
-      const fromIdVal = new Validators("from._id", from._id, "string").validate();
-      const fromOrder = new Validators("from.ordenacao", from.ordenacao, "number").validate();
-      const toIdVal = new Validators("to._id", to._id, "string").validate();
-      const toOrder = new Validators("to.ordenacao", to.ordenacao, "number").validate();
-      if (!fromIdVal.isValid) {
-        throw new InvalidParameter(fromIdVal);
+        if (values.nome) {
+          body.nome = values.nome;
+        }
+
+        return body;
+      })
+      .parse(req.body);
+
+      if (!Object.values(data).length) {
+        throw new BadRequestError("Nenhum dado de atualização foi informado.")
       }
-      if (!fromOrder.isValid) {
-        throw new InvalidParameter(fromOrder);
-      }
-      if (!toIdVal.isValid) {
-        throw new InvalidParameter(toIdVal);
-      }
-      if (!toOrder.isValid) {
-        throw new InvalidParameter(toOrder);
-      }
-      await Category.findByIdAndUpdate(from._id, { "ordenacao": to.ordenacao });
-      await Category.findByIdAndUpdate(to._id, { "ordenacao": from.ordenacao });
+
+      await this.repository.update(id, data);
+      
       return ApiResponse.success().send(res);
+
     } catch (e) {
       next(e);
     }
   }
 
-  static async delete(req: Request, res: Response, next: NextFunction) {
+  updateOrdenation = async (req: Request, res: Response, next: NextFunction) => {
     try {
-      let id = req.body.id;
-      const validation = new Validators("id", id, "string");
-      if (!validation.validate().isValid) {
-        throw new InvalidParameter(validation.validate());
-      }
-      if (await ProductController.productsHasCategory(id)) {
-        return ApiResponse.badRequest('Categoria não pode ser excluida se possuir produtos cadastrados').send(res);
-      }
-      await Category.findByIdAndDelete(id);
+      
+      const storeCode = idValidation.parse(req.params.storeCode);
+
+      const data = z.array(z.object({
+        id: idValidation,
+        ordenacao: z.number().min(1)
+      })).parse(req.body);
+
+      await this.repository.updateOrdenation(storeCode, data)
+
       return ApiResponse.success().send(res);
+
+    } catch (e) {
+      next(e);
+    }
+  }
+
+  delete = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const id = idValidation.parse(req.params.id);
+
+      const category = await this.repository.deleteOne(id);
+
+      return ApiResponse.success(category, 200).send(res);
     } catch (e) {
       next(e);
     }
