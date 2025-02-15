c5b420d1e15eea48f245cbef0d30ce4040d43849
Leandro <leandrosantosconceicao@gmail.com>
2025-02-15 11:38:01 -0300

    Interface do repository de categorias de produto

---
+++ src/domain/interfaces/ICategoryRepository.ts
@@ -0,0 +1,23 @@
+import { ICategory } from "../../models/Categories";
+
+export default interface ICategoryRepository {
+
+    findAll(storeCode: string, id?: string, nome?: string) : Promise<Array<ICategory>>
+
+    findOne(id: string) : Promise<ICategory>
+
+    add(category: ICategory) : Promise<ICategory>
+
+    deleteOne(id: string) : Promise<ICategory>
+
+    update(id: string, data: Partial<{
+        nome: string,
+        image: string
+      }>
+    ) : Promise<ICategory>
+
+    updateOrdenation(storeCode: string, data: Array<{
+        id?: string,
+        ordenacao?: number
+    }>) : Promise<void>
+}
