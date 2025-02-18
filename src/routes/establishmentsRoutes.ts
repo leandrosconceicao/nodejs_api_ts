508371b33d64f2caa74b61d608b9f2610198b1e7
Leandro <leandrosantosconceicao@gmail.com>
2025-02-17 21:38:19 -0300

    Ajustando controller para utilizar a injeção de dependencia

--- src/routes/establishmentsRoutes.ts
+++ src/routes/establishmentsRoutes.ts
@@ -3,11 +3,18 @@ import Endpoints from "../models/Endpoints";
 import validateToken from "../middlewares/tokenController";
 import EstablishmentsController from "../controllers/establishments/establishmentController";
 import paginationAndFilters from "../middlewares/paginationAndFilters";
+import { container } from "tsyringe";
+import IEstablishmentRepository from "../domain/interfaces/IEstablishmentRepository";
+import MongoEstablishmentRespository from "../repository/mongoEstablishmentRepository";
+import superUserTokenValidation from "../middlewares/superUserTokenValidation";
+
+container.resolve<IEstablishmentRepository>(MongoEstablishmentRespository);
+
+const establishmentController = container.resolve(EstablishmentsController);
 
 export default Router()
-    .get(Endpoints.establishments, validateToken, EstablishmentsController.findAll, paginationAndFilters)
-    .get(`${Endpoints.establishments}/:id`, EstablishmentsController.findOne)
-    .post(Endpoints.establishments, validateToken, EstablishmentsController.add)
-    .delete(`${Endpoints.establishments}/:id`, validateToken, EstablishmentsController.delete)
-    .put(`${Endpoints.establishments}/:id`, EstablishmentsController.put)
-    // .patch(`${Endpoints.establishments}/upload_logo/:id`, EstablishmentsController.updateLogo)
+    .get(Endpoints.establishments, validateToken, establishmentController.findAll, paginationAndFilters)
+    .get(`${Endpoints.establishments}/:id`, establishmentController.findOne)
+    .post(Endpoints.establishments, validateToken, establishmentController.add)
+    .delete(`${Endpoints.establishments}/:id`, superUserTokenValidation, establishmentController.delete)
+    .put(`${Endpoints.establishments}/:id`, validateToken, establishmentController.put)
