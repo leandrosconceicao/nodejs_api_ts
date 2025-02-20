3c66b88bd09e30a3f8331e74cefccbe38e1bc72a
Leandro <leandrosantosconceicao@gmail.com>
2025-02-20 12:21:23 -0300

    Implementando funcionalidade de notificação de usuários

--- src/domain/interfaces/ICloudService.ts
+++ src/domain/interfaces/ICloudService.ts
@@ -1,6 +1,9 @@
 export default interface ICloudService {
-    uploadFile(data: {
-        path?: string,
-        data?: string
-    }) : Promise<string>
+    
+    uploadFile(data: {path?: string, data?: string}) : Promise<string>
+
+    notifyUsers(token: string, title?: string, body?: string, data?: {
+        [key: string]: string;
+    }) : Promise<void>
+
 }
--- src/utils/cloud/cloudService.ts
+++ src/utils/cloud/cloudService.ts
@@ -1,6 +1,8 @@
 import { delay, injectable, registry } from "tsyringe";
 import ICloudService from "../../domain/interfaces/ICloudService";
 import {getStorage, getDownloadURL} from "firebase-admin/storage";
+import { messaging } from "firebase-admin";
+import ErrorAlerts from "../errorAlerts";
 
 @injectable()
 @registry([
@@ -10,6 +12,39 @@ import {getStorage, getDownloadURL} from "firebase-admin/storage";
     }
 ])
 export default class CloudService implements ICloudService {
+
+    async notifyUsers(token: string, title?: string, body?: string, data?: {[key: string]: string}): Promise<void> {
+        try {
+            const instance = messaging();
+            await instance.send({
+                token: token,
+                data: data,
+                notification: {
+                    title,
+                    body
+                },
+                android: {
+                    priority: "high",
+                },
+                apns: {
+                    payload: {
+                        aps: {
+                            contentAvailable: true,
+                        },
+                    },
+                    headers: {
+                        "apns-push-type": "background",
+                        "apns-priority": "5", // Must be `5` when `contentAvailable` is set to true.
+                        "apns-topic": "io.flutter.plugins.firebase.messaging", // bundle identifier
+                    },
+                },
+            })
+        } catch (e) {
+            ErrorAlerts.sendDefaultAlert(e);
+            return undefined;
+        }
+    }
+
     async uploadFile(data: { path?: string; data?: string; }): Promise<string> {
         const bucket = getStorage().bucket();
         const imageBuffer = Buffer.from(data.data, "base64");
