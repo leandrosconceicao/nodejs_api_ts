d4d7eb8ab22cb2a60b0a5224d7d7e8e11367ae1f
Leandro <leandrosantosconceicao@gmail.com>
2025-02-22 11:49:24 -0300

    fix: Ajustando dependencias

--- src/routes/accountRoutes.ts
+++ src/routes/accountRoutes.ts
@@ -2,14 +2,21 @@ import express from "express";
 import Endpoints from "../models/Endpoints";
 import AccountsController from "../controllers/accounts/accountsController";
 import paginationAndFilters from "../middlewares/paginationAndFilters";
-import validateToken from "../middlewares/tokenController";
+import validateToken, { TokenController } from "../middlewares/tokenController";
+import { container } from "tsyringe";
+import IAccountRepository from "../domain/interfaces/IAccountRepository";
+import MongoAccountRepository from "../repository/mongoAccountRepository";
+
+container.resolve<IAccountRepository>(MongoAccountRepository);
+const accountController = container.resolve(AccountsController);
+const tokenController = container.resolve(TokenController)
 
 export default express.Router()
-    .get(Endpoints.accounts, validateToken, AccountsController.findAll, paginationAndFilters)
-    .get(`${Endpoints.accounts}/:id`, validateToken, AccountsController.findOne)
-    .get(`${Endpoints.accounts}/v2/:id`, validateToken, AccountsController.findOneV2)
-    .post(Endpoints.accounts, validateToken, AccountsController.addNew)
-    .patch(`${Endpoints.accounts}/manage_account_tip/:storeCode/:accountId`, validateToken, AccountsController.manageAccountTip)
-    .put(`${Endpoints.accounts}/manage_status/:id`, validateToken, AccountsController.manageStatus)
-    .put(`${Endpoints.accounts}/:id`, validateToken, AccountsController.edit)
-    .delete(`${Endpoints.accounts}`, validateToken, AccountsController.del)
+    .get(Endpoints.accounts, validateToken, accountController.findAll, paginationAndFilters)
+    .get(`${Endpoints.accounts}/:id`, validateToken, accountController.findOne)
+    .get(`${Endpoints.accounts}/v2/:id`, validateToken, accountController.findOne)
+    .post(Endpoints.accounts, tokenController.userValidation, accountController.addNew)
+    .patch(`${Endpoints.accounts}/manage_account_tip/:storeCode/:accountId`, validateToken, accountController.manageAccountTip)
+    .put(`${Endpoints.accounts}/manage_status/:id`, validateToken, accountController.manageStatus)
+    .put(`${Endpoints.accounts}/:id`, validateToken, accountController.edit)
+    .delete(`${Endpoints.accounts}/:id`, validateToken, accountController.del)
