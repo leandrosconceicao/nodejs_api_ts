45374f6d41ef48e126f93194033d78cb7c65f085
Leandro <leandrosantosconceicao@gmail.com>
2026-01-30 20:17:49 -0300

    fix: Ajustando rotas e parametros

--- src/domain/types/IDeliveryOrder.ts
+++ src/domain/types/IDeliveryOrder.ts
@@ -1,5 +1,5 @@
 import { z } from "zod"
-import { clientDeliveryAddressValidation, clientsBasicInfoValidation, IClient, IClientAddress, IClientBasicInfo } from "../../models/Clients"
+import { clientsBasicInfoValidation, IClientBasicInfo } from "../../models/Clients"
 import MongoId from "../../models/custom_types/mongoose_types"
 import { IOrderProduct, orderProductValidation, OrderStatus } from "../../models/Orders"
 import { idValidation } from "../../utils/defaultValidations"
@@ -11,20 +11,21 @@ export const deliveryOrdersValidation = z.object({
     deliveryTax: z.number(),
     status: z.nativeEnum(OrderStatus).default(OrderStatus.pending),
     paymentMethod: idValidation,
+    deliveryDistrictId: idValidation,
     products: z.array(orderProductValidation).nonempty()
 })
 
 export const deliveryOrdersSearchValidation = z.object({
-    storeCode: idValidation,
     from: z.string().datetime({
         offset: true
-    }),
+    }).optional(),
     to: z.string().datetime({
         offset: true
-    }),
+    }).optional(),
     orderId: idValidation.optional(),
     status: z.nativeEnum(OrderStatus).optional(),
     paymentMethod: idValidation.optional(),
+    clientPhoneNumber: z.string().optional()
 })
 
 export const deliveryOrdersUpdateValidation = z.object({
@@ -36,24 +37,25 @@ export const deliveryOrdersUpdateValidation = z.object({
 export interface ISearchDeliveryOrder {
     _id: string,
     orderId: string,
-    storeCode: string,
     from: string,
     to: string,
     status: string,
-    paymentMethod: string
+    paymentMethod: string,
+    clientPhoneNumber: string
 }
 
 export interface IDeliveryOrder {
     _id?: string | MongoId,
+    deliveryDistrictId: string | MongoId,
     deliveryTax?: number,
     subTotal?: number,
     orderId?: string | MongoId,
     storeCode: string | MongoId,
-    createdAt?: Date | string,
+    createdAt?: Date,
     client: IClientBasicInfo,
     status: OrderStatus,
     paymentMethod: string,
     paymentMethodDetail?: any,
     establishmentDetail?: IEstablishments,
-    products: Array<IOrderProduct>
+    products: Array<IOrderProduct>,
 }
