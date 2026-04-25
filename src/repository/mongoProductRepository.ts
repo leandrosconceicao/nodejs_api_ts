c1a133cc3276fbb4a4187287a2f985208a81c4ce
Leandro <leandrosantosconceicao@gmail.com>
2026-04-25 15:06:33 -0300

    fix: refatorações

--- src/repository/mongoProductRepository.ts
+++ src/repository/mongoProductRepository.ts
@@ -38,7 +38,7 @@ export class MongoProductRepository implements IProductRepository {
         
         const company = await this.findByCompanyFilter(storeCode, productId);
 
-        if (!company.images.some((e) => e.thumbnail)) {
+        if (!(company.images?.some((e) => e.thumbnail) ?? false)) {
             file.thumbnail = true;
         }
 
@@ -148,7 +148,7 @@ export class MongoProductRepository implements IProductRepository {
         
         const product = await this.findByCompanyFilter(storeCode, productId);
 
-        const imageIndex = product.images.findIndex(img => img.filename === file.filename && img.link === file.link);
+        const imageIndex = product.images?.findIndex(img => img.filename === file.filename && img.link === file.link);
 
         if (imageIndex === -1) {
             throw new NotFoundError('Image not found');
