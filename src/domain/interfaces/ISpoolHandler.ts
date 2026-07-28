678c03c7979ea9518495c092a6967ef8b880fa50
Leandro <leandrosantosconceicao@gmail.com>
2026-07-28 19:51:15 -0300

    feat: implementando a impressão de comprovante de entrega

--- src/domain/interfaces/ISpoolHandler.ts
+++ src/domain/interfaces/ISpoolHandler.ts
@@ -5,7 +5,8 @@ export default interface ISpoolHandler {
     prepareData(data: IPrinterSpool) : Promise<IPrinterSpool>;
     prepareReceiptData(spool: IPrinterSpool) : Promise<IPrinterSpool>;
     prepareOrderData(data: IPrinterSpool) : Promise<IPrinterSpool>;
-    prepareCashRegisterData(data: IPrinterSpool) : Promise<IPrinterSpool>;    
+    prepareCashRegisterData(data: IPrinterSpool) : Promise<IPrinterSpool>;
+    prepareDeliveryData(data: IPrinterSpool) : Promise<IPrinterSpool>;
     removerAcentos(texto: string) : string
     formatNumber(value: number) : string
 }
